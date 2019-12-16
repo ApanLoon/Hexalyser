19888bc3c55ef2e0c4b313769ac4a2903ff12669
ApanLoon <apanloon@live.co.uk>
2019-12-16 13:38:39 +0100

    Generalised ElementView - Now uses a UserControl with a ValueContent property. This makes it easier to change the name and count text blocks to editable text boxes.

---
+++ src/Hexalyser/Views/ElementView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Hexalyser.Views
+{
+    /// <summary>
+    /// Interaction logic for ElementView.xaml
+    /// </summary>
+    public partial class ElementView : UserControl
+    {
+
+        public object ValueContent
+        {
+            get => (object)GetValue(ValueContentProperty);
+            set => SetValue(ValueContentProperty, value);
+        }
+
+        public static readonly DependencyProperty ValueContentProperty = DependencyProperty.Register
+        (
+            "ValueContent",
+            typeof(object),
+            typeof(ElementView),
+            new PropertyMetadata(null)
+        );
+
+        public Visibility CountVisibility
+        {
+            get => (Visibility)GetValue(CountVisibilityProperty);
+            set => SetValue(CountVisibilityProperty, value);
+        }
+
+        public static readonly DependencyProperty CountVisibilityProperty = DependencyProperty.Register
+        (
+            "CountVisibility",
+            typeof(Visibility),
+            typeof(ElementView),
+            new PropertyMetadata(null)
+        );
+
+        public ElementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
